509b1cdd740e5e23f6f9ccea4d2140ad4a334357
Makabaka1880 <makabaka1880@outlook.com>
2025-06-29 14:58:09 +0800

    feat: changed route to /today, added reason for grayscale

--- Sources/EpitaphBackend/Controllers/GraydateController.swift
+++ Sources/EpitaphBackend/Controllers/GraydateController.swift
@@ -23,19 +23,26 @@ struct GraydateController: RouteCollection {
         let graydates = routes
             .grouped("grayscale")
 
-        graydates.get("is-today", use: is_today)
+        graydates.get("today", use: today)
     }
 
-    func is_today(_ req: Request) async throws -> some AsyncResponseEncodable {
+    func today(_ req: Request) async throws -> some AsyncResponseEncodable {
         let today = Date().startOfDay
         let tomorrow = Date().endOfDay
 
-        let exists = try await req.db.query(Graydate.self)
+        let graydate = try await Graydate.query(on: req.db)
             .filter(\.$date, .greaterThanOrEqual, today)
             .filter(\.$date, .lessThanOrEqual, tomorrow)
             .first()
 
-        return ["today": exists != nil]
-    }
+        struct GrayscaleResponse: Content {
+            var gray: Bool
+            var reason: String?
+        }
 
+        return GrayscaleResponse(
+            gray: graydate != nil,
+            reason: graydate?.reason
+        )
+    }
 }
