82ef65ba73e1fa59c33717fb33b7579c2590cf7f
Makabaka1880 <makabaka1880@outlook.com>
2025-06-30 16:03:49 +0800

    patch: allowed for nested action path

--- Sources/EpitaphBackend/Controllers/ResourcesController.swift
+++ Sources/EpitaphBackend/Controllers/ResourcesController.swift
@@ -12,20 +12,23 @@ import Foundation
 struct ResourcesController: RouteCollection {
 	func boot(routes: any RoutesBuilder) throws {
 		let resources = routes.grouped("resources")
-		let badges = resources.grouped("badges", ":repo" ,":badge")
+		let badges = resources.grouped("badges", ":repo", "**")
 		let bucket = resources.grouped("bucket") // under dev
 
 		badges.get(use: getBadge)
 	}
 
 	func getBadge(_ req: Request) async throws -> Response {
-		guard let badge = req.parameters.get("badge") else {
-			throw Abort(.badRequest, reason: "badge parameter missing")
-		}
 		guard let repo = req.parameters.get("repo") else {
-			throw Abort(.badRequest, reason: "repo parameter missing")
+			throw Abort(.badRequest)
 		}
+		let badgeParts = req.parameters.getCatchall()
+		guard !badgeParts.isEmpty else {
+			throw Abort(.badRequest)
+		}
+		let badge = badgeParts.joined(separator: "/")
 
+	
 		let url = "https://github.com/makabaka1880/\(repo)/actions/workflows/\(badge)/badge.svg"
 		let clientResponse = try await req.client.get(URI(string: url)) { req in
 			req.headers.replaceOrAdd(name: .authorization, value: "token \(SecretsManager.get(.badgeFetchPAT) ?? "")")
